c4e17afa919de2cf352d68b1ba1def53d048f357
likecloud0711 <likecloud0711@gmail.com>
2022-12-05 16:25:30 +0900

    first commit!!

---
+++ src/main/java/com/work/TholDeskProjectApplication.java
@@ -0,0 +1,13 @@
+package com.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TholDeskProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TholDeskProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+<title>spring 시작!!</title>
+</head>
+ <body>
+  ${hello}
+ </body>
+</html>
